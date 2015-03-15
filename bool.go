cbfbc3d872273e9a2a54118c50b3aabe4c3ee4c8
Herbert G. Fischer <herbert.fischer@gmail.com>
2015-03-14 22:56:02 -0300

    Add tests to Bool

--- bool.go
+++ bool.go
@@ -2,13 +2,16 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"os"
 )
 
 type Bool bool
 
+var BoolWriter io.Writer = os.Stdout
+
 func (b Bool) Printf(s string, a ...interface{}) {
 	if b {
-		fmt.Fprintf(os.Stdout, s, a...)
+		fmt.Fprintf(BoolWriter, s, a...)
 	}
 }
